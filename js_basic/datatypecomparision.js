c5b8a6b95a0efa894bdc6b2dbc9023d5a7bfc51c
Manrajsingh17 <130748759+Manrajsingh17@users.noreply.github.com>
2024-09-23 04:43:05 +0000

    idial and not idial datatype

---
+++ js_basic/datatypecomparision.js
@@ -0,0 +1,22 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+//  below conversion are not ideal  conversion becouse they have diff data type ans its not a good practice
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === 
+
+console.log("2" === 2);
