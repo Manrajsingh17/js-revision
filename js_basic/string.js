15b324a1a70f6f9e42ee5aca53a005f80242bcf1
Manraj Rathore <130748759+Manrajsingh17@users.noreply.github.com>
2024-10-05 17:53:27 +0000

    string & there methods

---
+++ js_basic/string.js
@@ -0,0 +1,18 @@
+const name  = "manraj";
+const sirname = "singh";
+
+console.log('my name is ${name}');
+const gamename = new String('manraj')
+console.log(gamename[4]);
+console.log(gamename.length);
+console.log(gamename.toLocaleLowerCase());
+console.log(gamename.toLocaleUpperCase());
+console.log(gamename.charAt(2));
+console.log(gamename.indexOf('n'));
+const newvar  = gamename.substring(0,4)
+console.log(console.log(newvar));
+const anotherstring = gamename.slice(2,4);
+console.log(anotherstring);
+const anotherstring2 = "   manraj   "
+console.log(anotherstring2);
+console.log(anotherstring2.trim())
