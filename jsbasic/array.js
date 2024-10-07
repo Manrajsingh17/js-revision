b656709d80485d1aee29d4743a97a936b7b330c7
Manraj Rathore <130748759+Manrajsingh17@users.noreply.github.com>
2024-10-07 07:04:55 +0000

    about array

---
+++ jsbasic/array.js
@@ -0,0 +1,17 @@
+console.log("array")
+
+const myarray = [1,3,5,7,'name',1.2,'a'] //]include any type of data as a Element
+//it can be resize
+
+
+console.log(myarray[3])
+
+// array methods
+
+myarray.push(3)
+myarray.push(2)
+myarray.pop() //laast value will remove
+myarray.unshift(5)
+// myarray.shift()
+
+console.log(myarray)
